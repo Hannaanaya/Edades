05be9293a170c5847f1818f1fcd255a14004d6ac
Hannaanaya <hannaanaya0511@gmail.com>
2021-08-16 13:46:25 +0200

    Calcular el total de las edades con c#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Arreglo
+{
+    class Arreglo
+    {
+        int[] edades;
+        int suma = 0;
+        public void recibe()
+        {
+            edades = new int[10];
+            for (int i = 0; i < edades.Length; i++)
+            {
+                Console.Write("Ingrese la edad: " + (i + 1) + ": ");
+                edades[i] = int.Parse(Console.ReadLine());
+                suma += edades[i];
+            }
+            Console.Write("\nCalculando el promedio... ");
+            //INGRESA AQUÍ TU CÓDIGO
+            Console.Write("\n");
+            Console.Write("\nEl total de las edades es: " + suma);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Arreglo alumnos = new Arreglo();
+            alumnos.recibe();
+            Console.ReadKey();
+        }
+    }
+}
